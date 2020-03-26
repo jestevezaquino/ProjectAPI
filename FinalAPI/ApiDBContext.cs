9bf1e67f989628b8aa0296b0b42dda3c87ae7697
Jesus <Ing.estevezaquino@gmail.com>
2020-03-26 18:48:34 -0400

    Se creo la clase ApiDBContext.cs la cual hereda de DbContext. Dentro de la misma sobreescribimos un metodo perteneciente a DbContext llamado OnModelCreating, donde inicializamos el mapeo que se realizo anteriormente a los modelos. En ApiDbContext tambien se definieron propiedades de tipo DbSet<nombreModelo>, las cuales representaran las tablas de la base de datos en nuestra aplicacion.

---
+++ FinalAPI/ApiDBContext.cs
@@ -0,0 +1,32 @@
+﻿using FinalAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalAPI
+{
+    public class ApiDBContext : DbContext
+    {
+        public ApiDBContext(DbContextOptions options)  : base (options)
+        {
+
+        }
+
+        public DbSet<Producto> Producto { get; set; }
+        public DbSet<Proveedor> Proveedor { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Stock> Stock { get; set; }
+        public DbSet<Factura> Factura { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modeloCreador) 
+        {
+            new Producto.Mapeo(modeloCreador.Entity<Producto>());
+            new Proveedor.Mapeo(modeloCreador.Entity<Proveedor>());
+            new Cliente.Mapeo(modeloCreador.Entity<Cliente>());
+            new Stock.Mapeo(modeloCreador.Entity<Stock>());
+            new Factura.Mapeo(modeloCreador.Entity<Factura>());
+        }
+    }
+}
