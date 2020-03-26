b0de3e7f9f9ca147148dc4dc0e5364d53c2132df
Jesus <Ing.estevezaquino@gmail.com>
2020-03-26 17:39:26 -0400

    Se creo la clase modelo Proveedor.cs y se definieron sus propiedades.

---
+++ FinalAPI/Models/Proveedor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalAPI.Models
+{
+    public class Proveedor
+    {
+        public int ProveedorID { get; set; }
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string  Telefono { get; set; }
+        public string Email { get; set; }
+    }
+}
