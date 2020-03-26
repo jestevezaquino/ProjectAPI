8957c1999a40fcb437bc926eaf94ffd34e1d8ea2
Jesus <Ing.estevezaquino@gmail.com>
2020-03-26 18:00:32 -0400

    Se creo la clase modelo Factura.cs y se le establecieron sus propiedades.

---
+++ FinalAPI/Models/Factura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalAPI.Models
+{
+    public class Factura
+    {
+        public int FacturaID { get; set; }
+        public int ClienteID { get; set; }
+        public Cliente Cliente { get; set; }
+        public int CantidadProductos { get; set; }
+        public decimal SubTotal { get; set; }
+        public int DescuentoPorciento { get; set; }
+        public decimal Total { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
