26845b5997895666995c5890e089cb99ebec0f84
Jesus <Ing.estevezaquino@gmail.com>
2020-03-26 17:49:04 -0400

    Se creo la clase modelo Stock.cs y se establecieron sus propiedades.

---
+++ FinalAPI/Models/Stock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalAPI.Models
+{
+    public class Stock
+    {
+        public int StockID { get; set; }
+        public int ProductoID { get; set; }
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+        public int ProveedorID { get; set; }
+        public Proveedor Proveedor { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
