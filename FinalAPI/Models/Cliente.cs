698f9d0333a29fd793960ef2b433a99b3312ed8f
Jesus <Ing.estevezaquino@gmail.com>
2020-03-26 17:43:21 -0400

    Se creo la clase modelo Cliente.cs y se definieron sus propiedades.

---
+++ FinalAPI/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalAPI.Models
+{
+    public class Cliente
+    {
+        public int ClienteID { get; set; }
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Categoria { get; set; }
+    }
+}
