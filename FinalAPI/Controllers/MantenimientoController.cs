78d88d2fd6b36991b7cbf378ac607981f8201d53
Jesus <Ing.estevezaquino@gmail.com>
2020-03-26 20:11:58 -0400

    Inicialize el servicio Mantenimiento en el Startup.cs. Ademas se creo un controlador llamado MantenimientoController.

---
+++ FinalAPI/Controllers/MantenimientoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MantenimientoController : ControllerBase
+    {
+        private readonly MantenimientoService mantenimientoService;
+
+        public MantenimientoController(MantenimientoService _mantenimientoService) 
+        {
+            mantenimientoService = _mantenimientoService;
+        }
+
+        [Route("obtener_productos")]
+        public IActionResult ObtenerProductos() 
+        {
+            var resultado = mantenimientoService.ObtenerProductos();
+            return Ok(resultado);
+        }
+    }
+}
--- FinalAPI/Startup.cs
+++ FinalAPI/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FinalAPI.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,6 +29,7 @@ namespace FinalAPI
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<ApiDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConexionDB")));
+            services.AddTransient<MantenimientoService, MantenimientoService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
