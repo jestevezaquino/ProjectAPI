aa6b76d1da762f509299233f389cfd131b21f07b
Jesus <Ing.estevezaquino@gmail.com>
2020-04-04 21:18:11 -0400

    Se creo una clase modelo llamada Datos y un controlador con el mismo nombre.

---
+++ FinalAPI/Models/Datos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalAPI.Models
+{
+    public class Datos
+    {
+        public int Conteo { get; set; }
+        public int Sumatoria { get; set; }
+        public int Promedio { get; set; }
+        public int Max { get; set; }
+        public int Min { get; set; }
+    }
+}
